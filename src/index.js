860f6f3dc3b6a241168e00d85b3653c604359bc2
Toni <tony.juric02@gmail.com>
2025-05-30 23:35:58 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,127 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const articleData = [
+  {
+    name: "divinica 1",
+    photoName: "images/nausnica.jpg",
+    price: "6$",
+    category: "nausnice"
+  },
+  {
+    name: "divinica 11",
+    photoName: "images/nausnica.jpg",
+    price: "6$",
+    category: "nausnice"
+  },
+  {
+    name: "divinica 2",
+    photoName: "images/nausnica.jpg",
+    price: "7$",
+    category: "nausnice"
+  },
+  {
+    name: "divinica 3",
+    photoName: "images/narukvica.jpg",
+    price: "8$",
+    category: "narukvice"
+  },
+  {
+    name: "divinica 33",
+    photoName: "images/narukvica.jpg",
+    price: "8$",
+    category: "narukvice"
+  },
+  {
+    name: "divinica 4",
+    photoName: "images/narukvica.jpg",
+    price: "6$",
+    category: "narukvice"
+  },
+  {
+    name: "divinica 5",
+    photoName: "images/ogrlica.jpg",
+    price: "6$",
+    category: "ogrlice"
+  },
+  {
+    name: "divinica 10",
+    photoName: "images/ogrlica.jpg",
+    price: "6$",
+    category: "ogrlice"
+  },
+  {
+    name: "divinica 6",
+    photoName: "images/ogrlica.jpg",
+    price: "22$",
+    category: "ogrlice"
+  }
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <List />
+      <Footer />
+    </div>
+  );
+}
+
+
+function Header() {
+  return <header className='header'><h1>Divina Jewelry</h1></header>
+}
+
+function List() {
+  const categories = ["narukvice", "nausnice", "ogrlice"];
+
+  return (
+    <main className="category-section">
+      {categories.map((category) => (
+        <CategorySection
+          key={category}
+          title={category}
+          items={articleData.filter((item) => item.category === category)}
+        />
+      ))}
+    </main>
+  );
+}
+
+function CategorySection({ title, items }) {
+  return (
+    <div>
+      <h2>{title.charAt(0).toUpperCase() + title.slice(1)}</h2>
+      <ul className="articles">
+        {items.map((article) => (
+          <Article articleObj={article} key={article.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+function Article({articleObj}) {
+  return(
+    <li className='article'>
+      <img src={articleObj.photoName} alt={articleObj.name} />
+      <div>
+        <h3>{articleObj.name}</h3>
+        <p>{articleObj.price}</p>
+      </div>
+    </li>
+  )
+}
+
+function Footer() {
+  return <footer className='footer'>
+            <p>Instagram</p>
+          </footer>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +130,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
